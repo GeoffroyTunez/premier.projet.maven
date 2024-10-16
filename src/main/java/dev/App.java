cd318d1261f8567a8d5eb98701bcac301ba4e6fc
GeoffroyTunez <geoffroytunez@gmail.com>
2024-10-16 17:01:19 +0200

    commit fin de tp

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,16 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        String asciiArt1 = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt1);
     }
 }
